8867170731bcb6ae01b9c2798f483b56267c338e
Stefan Redfield <redfield.stefan@gmail.com>
2015-04-27 12:43:28 -0700

    Clarify language

--- eloquent-chapt-5/eloquentExcercises.js
+++ eloquent-chapt-5/eloquentExcercises.js
@@ -6,8 +6,8 @@
 var arrays = [[1, 2, 3], [4, 5], [6]];
 
 var reducer = function (array) {
-	return array.reduce(function(a, b) {
-		return a.concat(b);
+	return array.reduce(function(val, joined) {
+		return val.concat(joined);
 	})
 }
 
